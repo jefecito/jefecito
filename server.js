b39b5919dbe2686ce2991bb20a502eed50b4709d
finw3 <nestor.2005@gmail.com>
2018-03-11 15:12:45 -0300

    server.js file structure

--- server.js
+++ server.js
@@ -1,200 +1,225 @@
 /* jshint esversion: 6 */
 
-//====================================================================
-var bodyParser   = require('body-parser');
-var compression  = require('compression');
-var cookieParser = require('cookie-parser');
-var express      = require('express');
-var favicon      = require('serve-favicon');
-var fs           = require('fs');
-var http         = require('http');
-var https        = require('https');
-var logger       = require('morgan');
-var passport     = require('passport');
-var session      = require('express-session');
-var MongoStore   = require('connect-mongo')(session);
-var path         = require('path');
-var helmet       = require('helmet');
-var swaggerUi    = require('swaggerize-ui');
-var jsonfile     = require('jsonfile');
+// Variables
+// ====================================================================
+const cacheTime = 86400000 * 30
+const cookieTime = 1000 * 60 * 60 * 2
 
+// Require
+// ====================================================================
+const bodyParser   = require('body-parser')
+const compression  = require('compression')
+const cookieParser = require('cookie-parser')
+const express      = require('express')
+const favicon      = require('serve-favicon')
+const fs           = require('fs')
+const http         = require('http')
+const https        = require('https')
+const logger       = require('morgan')
+const passport     = require('passport')
+const session      = require('express-session')
+const MongoStore   = require('connect-mongo')(session)
+const path         = require('path')
+const helmet       = require('helmet')
+const swaggerUi    = require('swaggerize-ui')
+const jsonfile     = require('jsonfile')
 
-if(!fs.existsSync('./logs')) {
-  console.log('[+] logs folder created');
-  fs.mkdirSync('./logs');
+// Creates folder log
+if (!fs.existsSync('./logs')) {
+  console.log('[+] logs folder created')
+  fs.mkdirSync('./logs')
 }
 
+// Routes
+// =====================================================================
+const index = require('./routes/index')
+const login = require('./routes/login')
+const user  = require('./routes/user')
+const api   = require('./routes/api')
 
-//Routes
-//=====================================================================
-var index = require('./routes/index');
-var login = require('./routes/login');
-var user  = require('./routes/user');
-var api   = require('./routes/api');
+// Express configuration
+// ======================================================================
+const app = express()
 
-//Express configuration
-//======================================================================
-var app = express();
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.set('json spaces', 2)
+app.use(logger('dev'))
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('json spaces', 2);
-app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: cacheTime
+}))
 
-var cacheTime = 86400000*30;
-app.use(express.static(path.join(__dirname, 'public'),{maxAge: cacheTime}));
+// Error log managment
+// ======================================================================
+let errorLogStream = fs.createWriteStream(__dirname + '/logs/error.log', {
+  flags: 'a'
+})
 
-//ERROR LOG MANAGEMENT
-var errorLogStream = fs.createWriteStream(__dirname + '/logs/error.log', {flags: 'a'});
-
-//Error handling, avoiding crash
+// Error handling, avoiding crash
+// ======================================================================
 process.on('uncaughtException', (err, req, res, next) => {
-  var date = new Date();
-  console.error("+++++++ "+ date + " error found, logging event +++++++ ");
-  console.error(err.stack);
-  errorLogStream.write(date+ '\n'+ err.stack+'\n\n');
-  return;
-});
+  let date = new Date()
+  console.error(`+++++++ ${date} error found, logging event +++++++ `)
+  console.error(err.stack)
+  errorLogStream.write(`${date} \n ${err.stack} \n\n`)
+  return
+})
 
-//HELMET SECURITY Lib
-app.use(helmet());
+// Helment security lib
+// ======================================================================
+app.use(helmet())
 
-//RESPONSES DEFINITION
+// Response definition
+// ======================================================================
 app.use((req, res, next) => {
   res.success = (data, status) => {
-    return res.status((status) ? status : 200).send({
+    return res.status(status ? status : 200).send({
       success: true,
-      data: (data) ? data : '',
+      data: data ? data : '',
       error: null
-    });
-  };
+    })
+  }
+
   res.failure = (errorCode, errorMsg, status) => {
-    return res.status((status) ? status : 200).send({
+    return res.status(status ? status : 200).send({
       success: false,
       data: null,
       error: {
-        code: (errorCode) ? errorCode : -1,
-        message: (errorMsg) ? errorMsg : 'Unknown Error'
+        code: errorCode ? errorCode : -1,
+        message: errorMsg ? errorMsg : 'Unknown Error'
       }
-    });
-  };
-  next();
-});
+    })
+  }
+  next()
+})
 
-//Compress middleware to gzip content
-app.use(compression());
-//app.use(favicon(__dirname + '/public/img/favicon.png'));
+// Compress middleware to gzip content
+// ======================================================================
+app.use(compression())
+// app.use(favicon(__dirname + '/public/img/favicon.png'))
 
 //INITIAL JEFECITO CONFIG
-var file = './config/config.json';
-var cfg  = require(file);
+let file = './config/config.json'
+const cfg  = require(file)
 
-if(cfg.configured === false){
-  console.log("[+] You have to configure the App");
+if (cfg.configured === false) {
+  console.log('[+] You have to configure the App')
+
   app.use((req, res, next) => {
     jsonfile.readFile(file, (err, f) => {
-      if(err){
-        return res.failure(-1, err, 200);
-      }else{
-        if(f.configured === false){
-          if(req.method == "POST")
-            next();
-          else
-            return res.render("appConfig");
-        }else{
-          next();
+      if (err) {
+        return res.failure(-1, err, 200)
+      } else {
+        if (f.configured === false) {
+          if (req.method == "POST") {
+            next()
+          } else {
+            return res.render("appConfig")
+          }
+        } else {
+          next()
         }
-      }//else
-    });
-  });
-}else{
-  console.log("[+] App Configured, connecting to DB ..");
-  require('./config/mongo_conn');
+      } // if/else
+    })
+  })
+} else {
+  console.log('[+] App Configured, connecting to DB ...')
+  require('./config/mongo_conn')
 }
 
-require('./config/passport-local');
-require('./config/passport-facebook');
-require('./config/passport-google');
+require('./config/passport-local')
+require('./config/passport-facebook')
+require('./config/passport-google')
 
-//View engine setup
-//======================================================================
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser(cfg.appName));
+// View engine setup
+// ======================================================================
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(cookieParser(cfg.appName))
 
 app.use(session({
-   name: cfg.appName,
-   secret:'some_secret',
-   saveUninitialized: true,
-   resave: true,
-   cookie: { maxAge: 1000*60*60*2 },
-   store: new MongoStore({
-      url: 'mongodb://localhost/'+cfg.appName,
-      host: 'localhost',
-      collection: 'sessions',
-      autoReconnect: true,
-      clear_interval: 3600
-   })
-}));
+  name: cfg.appName,
+  secret:'some_secret',
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    maxAge: cookieTime
+  },
+  store: new MongoStore({
+    url: `mongodb://localhost/${cfg.appName}`,
+    host: 'localhost',
+    collection: 'sessions',
+    autoReconnect: true,
+    clear_interval: 3600
+  })
+}))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-//Routes usage =====================================================
-app.use('/', index);
-app.use('/', login);
-app.use('/', user);
-app.use('/', api);
+// Routes usage
+// ======================================================================
+app.use('/', index)
+app.use('/', login)
+app.use('/', user)
+app.use('/', api)
 
 app.use('/api-docs', (req, res) => {
-  res.json(require('./docs/api.json'));
-});
+  res.json(require('./docs/api.json'))
+})
 
 app.use('/internal/docs', swaggerUi({
   validatorUrl: null,
   docs: '/api-docs'
-}));
+}))
 
-//Disable server banner header
-app.disable('x-powered-by');
+// Disable server banner header
+// ======================================================================
+app.disable('x-powered-by')
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
+// ======================================================================
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if(process.env.NODE_ENV === 'development') {
+// Error Handlers
+// ======================================================================
+// Development error handler
+// Will print stacktrace
+// ======================================================================
+if (process.env.NODE_ENV === 'development') {
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler
+// No stacktraces leaked to user
+// ======================================================================
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-//Firing Up express
-//====================================================================
-app.set('port', process.env.PORT || 3000);
+// Firing Up express
+// ====================================================================
+app.set('port', process.env.PORT || 3000)
 
-var server = http.createServer(app).listen(app.get('port'), '127.0.0.1', () => {
-  console.log(cfg.appName+" server listening on port " + app.get('port'));
-});
+const server = http.createServer(app).listen(app.get('port'), '127.0.0.1', () => {
+  console.log(`${cfg.appName} server listening on port ${app.get('port')}`)
+})
 
-module.exports = app;
+module.exports = app
