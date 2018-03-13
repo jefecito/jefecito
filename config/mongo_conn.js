092237c13973198ea126b3e0fa5dec44cc159e71
Tito <nestor.2005@gmail.com>
2018-03-13 19:27:52 -0300

    fix create superadmin

--- config/mongo_conn.js
+++ config/mongo_conn.js
@@ -8,8 +8,6 @@ require('./models/alerts')
 require('./models/uplodas')
 require('./models/users')
 
-const User = mongoose.model('User')
-
 // mongoose.connect database
 mongoose.connect(`mongodb://127.0.0.1/${cfg.appName}`, err => {
   if(err)
@@ -20,35 +18,43 @@ mongoose.connect(`mongodb://127.0.0.1/${cfg.appName}`, err => {
   }
 });
 
-User.findOne({'local.creationMethod': 'superadmin'}, (err, doc) => {
+// create superadmin user
+const User = mongoose.model('User')
+const FILTER = {
+  'local.creationMethod': 'superadmin'
+}
+
+User.findOne(FILTER, (err, user) => {
   if(err)
     console.log('err', err);
   else {
-    if(!doc) {
-      var newUser = new User();
-
-      newUser.local.createdAt = Date.now();
-      newUser.local.username = 'admin';
-      newUser.local.email = 'admin@'+cfg.appURL;
-      newUser.local.password = newUser.generateHash('cr4fty_p4ssw0rd!');
-      newUser.local.roles = ['admin'];
-      newUser.local.isConfirmed = true;
-      newUser.local.creationMethod = 'superadmin';
+    if(!user) {
+      const newUser = new User({
+        local: {
+          createdAt: Date.now(),
+          username: 'admin',
+          email: `admin@${cfg.appURL}`,
+          password: newUser.generateHash('cr4fty_p4ssw0rd!'),
+          roles: ['admin'],
+          isConfirmed: true,
+          creationMethod: 'superadmin'
+        }
+      })
 
       newUser.save(err => {
         if(err)
-          console.log('err creating admin user', err);
+          console.log('err creating admin user', err)
         else {
-          console.log('');
-          console.log('======================');
-          console.log('Admin user created');
-          console.log('User: admin');
-          console.log('Email: ', 'admin@'+cfg.appURL);
-          console.log('Password:', 'Admin_'+cfg.appName);
-          console.log('======================');
-          console.log('');
+          console.log('')
+          console.log('======================')
+          console.log('Admin user created')
+          console.log('User: admin')
+          console.log('Email: ', `admin@${cfg.appURL}`)
+          console.log('Password:', 'cr4fty_p4ssw0rd')
+          console.log('======================')
+          console.log('')
         }
-      });
+      })
     }
   }
-});
+})
