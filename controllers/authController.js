d488abc505419a323f5cb6e217dddae73f78104a
Tito <nestor.2005@gmail.com>
2018-03-14 17:08:15 -0300

    add login social networks route/controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,12 @@
 /**
- * Inicio de Sesión
+ * Requires
  */
-exports.logIn = (req, res, next) => {
-  const passport = require('passport')
+const passport = require('passport')
+
+/**
+ * Inicio de Sesión Local
+ */
+exports.logInLocal = (req, res, next) => {
   passport.authenticate('local-login', (err, user, info) => {
     if (err) {
       return next(err)
@@ -20,6 +24,114 @@ exports.logIn = (req, res, next) => {
       return (req.user.local.roles.indexOf('admin') == -1) ?
         res.success({redirect: 'user', user: user}, 200) :
         res.success({redirect: 'dashboard', user: user}, 200)
+    })
+  })(req, res, next)
+}
+
+/**
+ * Inicio de Sesión Twitter
+ */
+exports.logInTwitter = (req, res, next) => {
+  passport.authenticate('twitter', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+
+    if (!user) {
+      return res.failure(-1, 'Authentication failed', 200)
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      } else {
+        if (user.local.roles.indexOf('admin') == -1) {
+          res.redirect('/usuario/perfil/'+req.user._id)
+        } else {
+          res.redirect('/dashboard')
+        }
+      }
+    })
+  })(req, res, next)
+}
+
+/**
+ * Inicio de Sesión Facebook
+ */
+exports.logInFacebook = (req, res, next) => {
+  passport.authenticate('facebook', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+
+    if (!user) {
+      return res.failure(-1, 'Authentication failed', 200)
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      } else {
+        if (user.local.roles.indexOf('admin') == -1) {
+          res.redirect('/usuario/perfil/'+req.user._id)
+        } else {
+          res.redirect('/admin/panel')
+        }
+      }
+    })
+  })(req, res, next)
+}
+
+/**
+ * Inicio de Sesión LinkedIn
+ */
+exports.logInLinkedIn = (req, res, next) => {
+  passport.authenticate('linkedin', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+
+    if (!user) {
+      return res.failure(-1, 'Authentication failed', 200)
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      } else {
+        if (user.local.roles.indexOf('admin') == -1) {
+          res.redirect('/usuario/perfil/'+req.user._id)
+        } else {
+          res.redirect('/admin/panel')
+        }
+      }
+    })
+  })(req, res, next)
+}
+
+/**
+ * Inicio de Sesión Google
+ */
+exports.logInGoogle = (req, res, next) => {
+  passport.authenticate('google', (err, user, info) => {
+    if (err) {
+      return next(err)
+    }
+
+    if (!user) {
+      return res.failure(-1, 'Authentication failed', 200)
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      } else {
+        if (user.local.roles.indexOf('admin') == -1) {
+          res.redirect('/usuario/perfil/'+req.user._id)
+        } else {
+          res.redirect('/admin/panel')
+        }
+      }
     })
   })(req, res, next)
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,12 +2,70 @@
 
 module.exports = application => {
   const authController = require('../controllers/authController')
+  const passport = require('passport')
   const mw = require('../middlewares/app')
 
+  /*
+   * Iniciar Sesión Local
+   */
   application
     .route('/login')
-    .post(mw.rateLimiter, authController.logIn)
+    .post(mw.rateLimiter, authController.logInLocal)
 
+  /*
+   * Ingresar con Twitter
+   */
+  application
+    .route('/auth/twitter')
+    .get(passport.authenticate('twitter'))
+
+  application
+    .route('/auth/twitter/callback')
+    .get(authController.logInTwitter)
+
+  /*
+   * Ingresar con Facebook
+   */
+  application
+    .route('/auth/facebook')
+    .get(passport.authenticate('facebook', {
+      scope : 'email'
+    }))
+
+  application
+    .route('/auth/facebook/callback')
+    .get(authController.logInFacebook)
+
+  /*
+   * Ingresar con LinkedIn
+   */
+  application
+    .route('/auth/linkedin')
+    .get(passport.authenticate('linkedin'))
+
+  application
+    .route('/auth/linkedin/callback')
+    .get(authController.logInLinkedIn)
+
+  /*
+   * Ingresar con Google
+   */
+  application
+    .route('/auth/google')
+    .get(passport.authenticate('google', {
+      scope: [
+        'profile',
+        'email'
+      ]
+    }))
+
+  application
+    .route('/auth/google/callback')
+    .get(authController.logInGoogle)
+
+  /*
+   * Cerrar Sesión
+   */
   application
     .route('/logout')
     .get(mw.rateLimiter, authController.logOut)
